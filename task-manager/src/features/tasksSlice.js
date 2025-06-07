974e0b6061a9a6d0505c9d4883f6e0fea3a55e7c
josuerushanika <josuerushanika@gmail.com>
2025-06-07 11:59:04 +0200

    Add stores

--- task-manager/src/features/tasksSlice.js
+++ task-manager/src/features/tasksSlice.js
@@ -1,1 +1,24 @@
-i
+import {createSlice } from '@reduxjs/toolkit'
+
+const initialState= {
+    tasks: [],
+}
+
+const taskSlice = createSlice ({
+    name: 'task',
+    initialState,
+
+    reducers: {
+        addTask: (state, action) => {
+            state.tasks.push({id: Date.now(), text: action.payload});
+        },
+
+        deleteTask: (state, action) => {
+            state.tasks = state.tasks.filter(task => task.id !== action.payload)
+        }
+    }
+})
+
+
+export const {addTask, deleteTask} = taskSlice.actions;
+export default taskSlice.reducer;
